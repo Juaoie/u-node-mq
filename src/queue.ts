85c017cbcef34f7d67f9f66f29aa012606998074
Juaoie <uaoie@qq.com>
2021-07-19 00:14:51 +0800

    修改问题

--- src/queue.ts
+++ src/queue.ts
@@ -1,3 +1,5 @@
+const promiseFun = (t?: number) => new Promise((res) => setTimeout(res, t));
+
 /**
  * 队列
  */
@@ -13,7 +15,7 @@ export class Queue {
    */
   pushQueueNews(news: any) {
     this.newsList.push(news);
-    this.sendMessageToConsumer()
+    this.sendMessageToAllConsumer();
   }
   /**
    * 获取队列名称
@@ -58,34 +60,41 @@ export class Queue {
      * 所以这里先假设消息确认都能正常返回，并且不报错
      */
     const askList: boolean[] = await Promise.all(listPromiseFun);
-    confirmNewsList.forEach((item, index) => {
+    confirmNewsList.forEach(async (item, index) => {
       //如果消息返回未被消费，则重新添加到消息列表中等待下一消息事件触发
       /**
        * TODO:
        * 如果消息未被消费
-       * 这里不间隔让消费者消费
-       * 为了避免死循环
+       * 这里将延迟一秒后再次发送给消费者
        * 所以这里只等待下一次发送消息事件触发
        */
-      if (Boolean(askList[index]) === false) this.pushQueueNews(item);
+      if (Boolean(askList[index]) === false) {
+        await promiseFun(1000);
+        this.pushQueueNews(item);
+      }
     });
   }
-   /**
+  /**
    * 发送消息给所有消费者
    */
-    async sendMessageToAllConsumer() {
-      //列表循环消费
-      const listPromiseFun = this.newsList.map((item: any) =>
-        consumer.consume(item)
-      );
-      //消息确认列表
-      const confirmNewsList: any[] = this.newsList;
-      this.newsList = [];
-      const askList: boolean[] = await Promise.all(listPromiseFun);
-      confirmNewsList.forEach((item, index) => {
-        if (Boolean(askList[index]) === false) this.pushQueueNews(item);
-      });
-    }
+  async sendMessageToAllConsumer() {
+    if (this.consumerList.length === 0) return;
+    //列表随机消费
+    const listPromiseFun = this.newsList.map((item: any) => {
+      const round = Math.round(Math.random() * (this.consumerList.length - 1));
+      return this.consumerList[round].consume(item);
+    });
+    //消息确认列表
+    const confirmNewsList: any[] = this.newsList;
+    this.newsList = [];
+    const askList: boolean[] = await Promise.all(listPromiseFun);
+    confirmNewsList.forEach(async (item, index) => {
+      if (Boolean(askList[index]) === false) {
+        await promiseFun(1000);
+        this.pushQueueNews(item);
+      }
+    });
+  }
   /**
    * 删除指定消费者
    * @param fun
