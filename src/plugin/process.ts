e416f2b49c5f0c7b3bcc2ca68936d1f9c8f65144
uaoie <uaoie@qq.com>
2021-12-29 22:18:36 +0800

    落下的一点

--- src/plugin/process.ts
+++ src/plugin/process.ts
@@ -1,4 +1,5 @@
 import UNodeMQ from "../core";
+import { Consume, Next } from "../core/consumer";
 import { Queue } from "../UNodeMQ";
 export default class Process<D> {
   unmq: UNodeMQ<D>;
@@ -6,11 +7,23 @@ export default class Process<D> {
   install(unmq: UNodeMQ<D>) {
     this.unmq = unmq;
   }
-  on() {
+  /**
+   *
+   * @param n 执行顺序，从小到大执行，相等的同时执行
+   */
+  on(n: number, consume: Consume<D>, payload?: any) {
     const queueName = Symbol(this.unmq.exchange.name);
     this.queueNameList.push(queueName);
-    new Queue({ name:queueName });
-    this.unmq.on();
+    const queue = new Queue<D>({ name: queueName, ask: true });
+    this.unmq.queueList.push(queue);
+    function consumeProxy(content: D, next: Next, payload?: any) {
+      const nextProxy=(n:number)=>{
+
+      }
+      consume(content, nextProxy, payload);
+      next(true);
+    }
+    this.unmq.on(queueName, consumeProxy, payload);
   }
   next() {}
   constructor() {}
