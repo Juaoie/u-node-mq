5ca0108b3fead490ead40a886c196432cab062b1
uaoie <uaoie@qq.com>
2021-12-30 17:47:19 +0800

    process插件功能

--- src/plugin/process.ts
+++ src/plugin/process.ts
@@ -1,29 +1,61 @@
 import UNodeMQ from "../core";
 import { Consume, Next } from "../core/consumer";
 import { Queue } from "../UNodeMQ";
-type NextProxy = (name: string) => void;
-type ProcessConsume<D> = (content: D, nextProxy: NextProxy, payload?: any) => void;
-export default class Process<D> {
-  unmq: UNodeMQ<D>;
-  install(unmq: UNodeMQ<D>) {
+export type NextProxy = (...queueNameList: string[]) => void;
+export type ProcessConsume = (queueName: string, nextProxy: NextProxy, payload?: any) => void;
+export default class Process {
+  unmq: UNodeMQ<string>;
+  install(unmq: UNodeMQ<string>) {
     this.unmq = unmq;
+    this.unmq.exchange.repeater = (queueName: string) => [queueName];
   }
   /**
    *
-   * @param n 执行顺序，从小到大执行，相等的同时执行
+   * @param contentList
+   * @returns
    */
-  on(name: string, consume: ProcessConsume<D>, payload?: any) {
-    const queue = new Queue<D>({ name, ask: true });
+  emit(...contentList: string[]) {
+    this.unmq.emit(...contentList);
+    return this;
+  }
+  /**
+   *
+   * @param name
+   * @param consume
+   * @param payload
+   */
+  on(name: string, consume: ProcessConsume, payload?: any) {
+    const queue = new Queue<string>({ name, ask: true });
     this.unmq.queueList.push(queue);
-    const consumeProxy = (content: D, next: Next, payload?: any) => {
-      const nextProxy = (name: string) => {
-        this.unmq.emit(); //这里要使用交换机的中继器分发数据到每个指定的队列
+    const consumeProxy = (content: string, next: Next, payload?: any) => {
+      next(true);
+      const nextProxy = (...nextQueueNameList: string[]) => {
+        this.unmq.emit(...nextQueueNameList); //这里要使用交换机的中继器分发数据到每个指定的队列
       };
       consume(content, nextProxy, payload);
-      next(true);
     };
     this.unmq.on(name, consumeProxy, payload);
+    return this;
   }
-  next() {}
+  /**
+   *
+   * @param queueName
+   * @param consume
+   * @returns
+   */
+  off(queueName: string | Symbol, consume: Consume<string>) {
+    this.unmq.off(queueName, consume);
+    return this;
+  }
+  /**
+   *
+   * @param queueName
+   * @param consume
+   * @param payload
+   */
+  once(queueName: string, consume: Consume<string>, payload?: any) {
+    this.unmq.once(queueName, consume, payload);
+    return this;
+  }
   constructor() {}
 }
