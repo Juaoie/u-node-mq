31752419e69f0d82940125fa3018c0db52a622ac
uaoie <uaoie@qq.com>
2022-04-11 20:17:30 +0800

    TODO

--- esbuild.script/dev.js
+++ esbuild.script/dev.js
@@ -4,7 +4,8 @@ const { dtsPlugin } = require("esbuild-plugin-d.ts");
 esbuild
   .build({
     entryPoints: ["./src/index.ts", "./src/plugins/iframe/index.ts", "./src/plugins/storage/index.ts"],
-    bundle: true,
+    bundle: false,
+    minify: true,
     outdir: "./",
   })
   .catch(() => process.exit(1));
