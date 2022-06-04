a36d9c835d995b8f7cc31c6c9c80263f26f45f51
uaoie <uaoie@qq.com>
2022-06-04 13:37:36 +0800

    pref:优化依赖项配置

--- esbuild.script/build.js
+++ esbuild.script/build.js
@@ -1,14 +1,12 @@
 const esbuild = require("esbuild");
-// const { dtsPlugin } = require("esbuild-plugin-d.ts");
 
 const buildType = ["browser", "node", "neutral"];
-
 async function buildMian() {
   const list = [];
   for (const type of buildType) {
     list.push(
       esbuild.build({
-        entryPoints: ["./src/index.ts"],
+        entryPoints: ["./src/index.ts"], //fileList.filter((item) => ["src\\adapter\\PiniaStorageAdapter.ts"].indexOf(item) === -1),
         bundle: true,
         outfile: "./dist/index." + type + ".min.js",
         minify: true,
@@ -21,74 +19,3 @@ async function buildMian() {
   Promise.all(list).catch(() => process.exit(1));
 }
 buildMian();
-
-////plugins
-const pluginsList = [
-  {
-    entryPoint: "./src/plugins/iframe/index.ts",
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    outfiles: {
-      node: "./dist/plugins/iframe/index.node.js",
-      browser: "./dist/plugins/iframe/index.browser.js",
-      neutral: "./dist/plugins/iframe/index.neutral.js",
-    },
-    // plugins: [dtsPlugin({ outDir: "./dist" })], //生成d.ts文件，拖慢了打包速度
-  },
-  {
-    entryPoint: "./src/operators/debounceTime.ts",
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    outfiles: {
-      node: "./dist/operators/debounceTime.node.js",
-      browser: "./dist/operators/debounceTime.browser.js",
-      neutral: "./dist/operators/debounceTime.neutral.js",
-    },
-    // plugins: [dtsPlugin({ outDir: "./dist" })], //生成d.ts文件，拖慢了打包速度
-  },
-  // {
-  //   entryPoint: "./src/plugins/storage/index.ts",
-  //   bundle: true,
-  //   minify: true,
-  //   sourcemap: true,
-  //   outfiles: {
-  //     node: "./dist/plugins/storage/index.node.js",
-  //     browser: "./dist/plugins/storage/index.browser.js",
-  //     neutral: "./dist/plugins/storage/index.neutral.js", //暂时无法打包
-  //   },
-  // },
-  // {
-  //   entryPoint: "./src/adapter/PiniaStorageAdapter.ts",
-  //   bundle: false,
-  //   minify: true,
-  //   sourcemap: true,
-  //   outfiles: {
-  //     node: "./dist/adapter/PiniaStorageAdapter.node.js",
-  //     browser: "./dist/adapter/PiniaStorageAdapter.browser.js",
-  //     neutral: "./dist/adapter/PiniaStorageAdapter.neutral.js",
-  //   },
-  // },
-];
-
-async function pluginsBuild() {
-  const list = [];
-  for (const item of pluginsList) {
-    for (const key in item.outfiles) {
-      list.push(
-        esbuild.build({
-          entryPoints: [item.entryPoint],
-          platform: key,
-          bundle: item.bundle,
-          outfile: item.outfiles[key],
-          minify: item.minify,
-          sourcemap: item.sourcemap,
-        })
-      );
-    }
-  }
-
-  Promise.all(list).catch(() => process.exit(1));
-}
-pluginsBuild();
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,50 @@
+/**
+ * 主包
+ */
+import UNodeMQ, { createUnmq, createQuickUnmq, QuickUNodeMQ, PluginInstallFunction } from "./core/UNodeMQ";
+export default UNodeMQ;
+export { createUnmq, createQuickUnmq, QuickUNodeMQ, PluginInstallFunction };
+
+/**
+ * 组件
+ */
 import Exchange from "./internal/Exchange";
 import Queue, { ConsumMode, Operator } from "./internal/Queue";
 import Consumer from "./internal/Consumer";
 import News from "./internal/News";
 import Logs from "./internal/Logs";
-
-import UNodeMQ, { createUnmq, createQuickUnmq, QuickUNodeMQ, PluginInstallFunction } from "./core/UNodeMQ";
-export default UNodeMQ;
-
-export { createUnmq, createQuickUnmq, QuickUNodeMQ, PluginInstallFunction };
-
 export { Exchange, Queue, Consumer, News, Logs };
 
 export { ConsumMode, Operator };
 
+/**
+ * plugins
+ */
+import IframePlugin from "./plugins/iframe/index";
+export { IframePlugin };
+
+/**
+ * operators
+ */
 export { map } from "./operators/map";
 export { debounceTime } from "./operators/debounceTime";
 export { throttleTime } from "./operators/throttleTime";
 export { task } from "./operators/task";
 export { newsTime } from "./operators/newsTime";
 
+/**
+ * 转换
+ */
 export const extend = Object.assign;
 export const objectToString = Object.prototype.toString;
 export const toTypeString = (value: unknown): string => objectToString.call(value);
+
+/**
+ * 类型判断
+ */
 export const isArray = Array.isArray;
 export const isMap = (val: unknown): val is Map<any, any> => toTypeString(val) === "[object Map]";
 export const isSet = (val: unknown): val is Set<any> => toTypeString(val) === "[object Set]";
-
 export const isDate = (val: unknown): val is Date => val instanceof Date;
 export const isFunction = (val: unknown): val is Function => typeof val === "function";
 export const isString = (val: unknown): val is string => typeof val === "string";
@@ -33,7 +52,6 @@ export const isNumber = (val: unknown): val is number => typeof val === "number"
 export const isBoolean = (val: unknown): val is boolean => typeof val === "boolean";
 export const isSymbol = (val: unknown): val is symbol => typeof val === "symbol";
 export const isObject = (val: unknown): val is Record<any, any> => val !== null && typeof val === "object";
-
 export const isPromise = <T = any>(val: unknown): val is Promise<T> => {
   return isObject(val) && isFunction(val.then) && isFunction(val.catch);
 };
