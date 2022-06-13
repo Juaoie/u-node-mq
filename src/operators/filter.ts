6f083fe9d0118bbb68d8d1c0ae1454116bc2b52e
uaoie <uaoie@qq.com>
2022-06-13 17:54:59 +0800

    feat:添加instant

--- src/operators/filter.ts
+++ src/operators/filter.ts
@@ -3,7 +3,7 @@ import { Operator, News } from "..";
 /**
  * filter 过滤
  * @param fun
- * @returns
+ * @returns boolean 返回值控制是否加入队列
  */
 export function filter<D>(fun: (res: any) => boolean | Promise<boolean>): Operator<D> {
   return {
